02731868d964956be94b88bc651e69daa0cdf207
Jack Michaud <mrmichaud42@gmail.com>
2020-03-13 10:04:04 -0400

    Spruce up the PlaylistListItem

---
+++ web_src/src/components/PlaylistListItem.tsx
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import {
+  Link
+} from 'react-router-dom';
+
+import 'babel-polyfill';
+import SpotifyIcon from './SpotifyIcon';
+
+interface IProps {
+    playlist: SpotifyApi.PlaylistObjectSimplified;
+}
+
+const PlaylistListItem = (props: IProps) => {
+
+  return (
+    <div>
+      <div className="flex flex-row mx-auto max-w-3xl p-3 text-white items-center">
+        <div className="flex-1">
+          <img className="w-full shadow-xs rounded" src={props.playlist.images[0].url} alt="" />
+        </div>
+        <div className="flex-1 px-3 bg-purple-700 sm:-ml-8 rounded flex flex-col">
+          <div className="uppercase mx-auto -mt-10 text-2xl bg-purple-600 p-2 rounded">
+            <Link to={`/playlist/${props.playlist.id}`}>Playlist</Link>
+          </div>
+          <h1 className="text-2xl">{props.playlist.name}</h1>
+          <h3>Owner: {props.playlist.owner.display_name}</h3>
+          <a className="transition" 
+            target="_blank"
+            href={props.playlist.external_urls.spotify}>
+            Link on Spotify <SpotifyIcon/>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PlaylistListItem;
