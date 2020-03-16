da31f6f99190310c18a41beef184d509f5a6df90
Tina Wu <tinawu0603@hotmail.com>
2020-03-16 13:03:51 -0400

    fix await response and added url prefix

--- web_src/src/services/PlaylistService.ts
+++ web_src/src/services/PlaylistService.ts
@@ -1,24 +1,24 @@
 export async function searchPlaylist(query: string): Promise<SpotifyApi.PlaylistObjectSimplified[]> {
     // generates URLSearchParams
     var searchParams = new URLSearchParams();
-    searchParams.set("q", `"` + encodeURI(query) + `"`);
+    searchParams.set("q", encodeURI(query));
     searchParams.set("type", "playlist");
     // call API with full URL
-    var fullURL: string = makeURL(searchParams);
-    const response = http.get(fullURL);
+    var fullURL: string = makeURL("/playlists/?", searchParams);
+    const response = await http.get(fullURL);
     // extracts playlists from response
-    const body: SpotifyApi.PlaylistSearchResponse = await (await response).json();
+    const body: SpotifyApi.PlaylistSearchResponse = await response.json();
     let playlists: SpotifyApi.PlaylistObjectSimplified[] = body.playlists.items
     return playlists;
 }
 
 const http = {
     async get(url: string) {
-        await fetch(url);
+        return await fetch(url);
     }
 }
 
 // Returns a full URL (Ex: http://localhost:8080/playlists/?q="hello"&type=playlist)
-function makeURL(searchParams: URLSearchParams): string {
-    return "http://localhost:8080" + searchParams.toString();
+function makeURL(prefix: string, searchParams: URLSearchParams): string {
+    return "http://localhost:8080" + prefix + searchParams.toString();
 }
