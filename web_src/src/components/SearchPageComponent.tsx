dca9def5fbbcc0d5760888afaa20592695a42db2
Jack Michaud <mrmichaud42@gmail.com>
2020-03-21 12:36:10 -0400

    Updated some styling on the search page

--- web_src/src/components/SearchPageComponent.tsx
+++ web_src/src/components/SearchPageComponent.tsx
@@ -18,19 +18,24 @@ class SearchPageComponent extends React.PureComponent<{}, IState> {
 
     render() {
         return (
-            <div className="text-purple-500" >
-                <p>Hi :D</p>
-                <input type="text" id="search-input" placeholder="Search playlists..." onChange={(e) => {
-                    this.setState({
-                        searchQuery: e.target.value
-                    });
-                }} />
-                <button type="submit" onClick={async () => {
-                    const listy = await searchPlaylist(this.state.searchQuery);
-                    this.setState({
-                        playlists: listy
-                    });
-                }}>Search Playlist</button>
+            <div className="items-center flex flex-col text-purple-500">
+              <span className="italic font-semibold text-2xl">
+                  Search 
+              </span>
+              <div className="px-3 flex flex-col">
+                  <input className="p-2 rounded text-center mb-1"
+                         type="text" id="search-input" placeholder="Search playlists..." onChange={(e) => {
+                        this.setState({
+                            searchQuery: e.target.value
+                        });
+                    }} />
+                    <button type="submit" onClick={async () => {
+                        const listy = await searchPlaylist(this.state.searchQuery);
+                        this.setState({
+                            playlists: listy
+                        });
+                    }}>Search Playlist</button>
+                </div>
                 {
                     this.state.playlists && this.state.playlists.length > 0 && this.state.playlists.map(playlist =>
                         <li key={playlist.id}>
