289978cbef6386af4fc0f113dd4dc0f5fabba832
Jack Michaud <mrmichaud42@gmail.com>
2020-03-23 09:08:32 -0700

    Switch to current playlist detail component

--- web_src/src/components/PlaylistDetailComponent.tsx
+++ web_src/src/components/PlaylistDetailComponent.tsx
@@ -1,57 +1,55 @@
-import React from 'react';
 
+import React from 'react';
+import {
+  Link
+} from 'react-router-dom';
+
+import TrackListItem from './TrackListItem';
 import 'babel-polyfill';
 import SpotifyIcon from './SpotifyIcon';
 
 interface IProps {
-  playlistPreview: SpotifyApi.PlaylistObjectSimplified;
-  playlist: SpotifyApi.PlaylistObjectFull;
-  loading: boolean;
+    playlist: SpotifyApi.PlaylistObjectFull;
 }
 
 const PlaylistDetailComponent = (props: IProps) => {
-  const playlist = props.playlistPreview;
-  const tracks = props.playlist?.tracks;
-  const loading = props.loading;
 
   return (
     <div>
-      <div className="flex sm:flex-row flex-col p-3 bg-purple-800 text-white items-center">
-        <div className="flex-1">
-          <img className="w-full shadow-xs rounded" src={playlist.images[0].url} alt="" />
-        </div>
-        <div className="flex-1 px-3 bg-purple-700 md:-ml-8 rounded flex flex-col">
-          <div className="uppercase mx-auto -mt-10 text-2xl bg-purple-600 p-2 rounded">
-            Playlist
+      <div className="bg-purple-800">
+        <div className="sm:flex sm:flex-row mx-auto max-w-3xl p-3 text-white items-center">
+          <div className="flex-1">
+            <img className="mx-auto sm:w-full shadow-xs rounded" src={props.playlist.images[0].url} alt="" />
           </div>
-          <h1 className="text-2xl">{playlist.name}</h1>
-          <h3>Owner: {playlist.owner.display_name}</h3>
-          <a className="transition" 
-             target="_blank"
-             href={playlist.external_urls.spotify}>
-            Link on Spotify <SpotifyIcon/>
-          </a>
+          <div className="flex-1 px-3 pb-4 bg-purple-700 sm:-ml-8 rounded flex flex-col shadow-xl z-1">
+            <div className="uppercase mx-auto -mt-10 text-2xl font-semibold bg-purple-600 p-2 rounded">
+              <Link to={`/playlist/${props.playlist.id}`}>Playlist</Link>
+            </div>
+            <h1 className="text-2xl">{props.playlist.name}</h1>
+            <h3>Owner: {props.playlist.owner.display_name}</h3>
+            <a className="transition" 
+              target="_blank"
+              href={props.playlist.external_urls.spotify}>
+              Link on Spotify <SpotifyIcon/>
+            </a>
+          </div>
         </div>
       </div>
+
+      { props.playlist.tracks?.items ?
       <div className="w-full flex flex-col">
-        <div className="text-3xl uppercase mx-auto inline-block border-b-2 border-purple-600 self-center">
+        <div className="text-3xl uppercase italic mx-auto inline-block self-center mt-5">
           Tracks
         </div>
         <div className="px-3">
           {
-            loading && <div className="italics">loading...</div>
-          }
-          {
-            tracks.items.map((track, idx) => (
-              <div key={idx}>
-                <span className="text-purple-600 pr-1">{ idx + 1}</span>
-                <span className="text-purple-700 pr-2">{ track.track.artists.map(a => a.name).join(',') }</span>
-                { track.track.name }
-              </div>
+            props.playlist.tracks.items.map((track, idx) => (
+              <TrackListItem key={idx} playlistIdx={idx} track={track}/>
             ))
           }
         </div>
-      </div>
+      </div> 
+      : <div className="italics text-purple-800 text-center">Loading...</div> }
     </div>
   )
 }
