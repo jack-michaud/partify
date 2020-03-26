58f6e51daf1a5b3c1d091496e8cd9fbc5c559352
Jack Michaud <mrmichaud42@gmail.com>
2020-03-26 09:54:03 -0700

    Use cors plugin on http api

--- backend_src/http_api/app.js
+++ backend_src/http_api/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const playlistRouter = require('./routes/playlists');
 
 const app = express();
+app.use(cors);
 
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/html');
