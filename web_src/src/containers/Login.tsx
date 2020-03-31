b83e093cfab7ee3eb2d6812f6afd060af184e771
Jack Michaud <mrmichaud42@gmail.com>
2020-03-31 15:10:56 -0700

    Separate login into container and component

--- web_src/src/containers/Login.tsx
+++ web_src/src/containers/Login.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import LoginComponent from '../components/LoginComponent';
+
 const Login = () => {
   return (
-    <div>
-      <a href={`https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=user-read-playback-state user-modify-playback-state user-read-currently-playing playlist-read-collaborative playlist-modify-public playlist-read-private&response_type=code&redirect_uri=${document.location.origin}/`}>
-        Log In
-      </a>
-    </div>
+    <>
+      <LoginComponent />
+    </>
   );
 }
 
