53a14fbabffdf1219a32b8c739e6016edc63176b
Jack Michaud <mrmichaud42@gmail.com>
2020-04-15 12:08:19 -0700

    Add reccomendations link to homepage

--- web_src/src/components/HomeComponent.tsx
+++ web_src/src/components/HomeComponent.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import PartifyLogo from './PartifyLogo';
 
 const HomeButton = (props) =>  (
-  <div className="flex items-center justify-center rounded-lg bg-purple-900 text-3xl text-white shadow-lg hover:bg-purple-800 transition-colors p-3 duration-200 mb-3">
+  <div className="flex items-center justify-center rounded-lg bg-purple-900 text-3xl text-white shadow-lg hover:bg-purple-800 transition-colors p-3 duration-200 m-3">
     { props.children }
   </div>
 )
@@ -15,10 +15,15 @@ const HomeComponent = () => {
         <div className="w-full flex justify-center absolute -mt-10" style={{zIndex: '-1'}}>
           <PartifyLogo animated={true} size="2xl" />
         </div>
-        <div className="md:w-9/12 sm:w-full h-24 mt-64 mx-10 grid grid-cols-1 md:grid-cols-2">
+        <div className="md:w-9/12 sm:w-full h-24 mt-64 mx-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           <Link to="/featured">
             <HomeButton>
               Featured
+            </HomeButton>
+          </Link>
+          <Link to="/recommendations">
+            <HomeButton>
+              Recommendations
             </HomeButton>
           </Link>
           <Link to="/search">
